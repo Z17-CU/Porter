118dbfd974179c37e2231c24d17775331c01f96a
Richard Anca Perurena <richard.anca.93@gmail.com>
2020-05-25 14:46:45 -0400

    [ADD] gradle to kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,199 @@
+import com.android.build.gradle.internal.api.BaseVariantOutputImpl
+import org.jetbrains.kotlin.konan.properties.Properties
+
+plugins {
+
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(29)
+
+    val versionPropsFile = file("version.properties")
+
+    if (versionPropsFile.canRead()) {
+        val versionProps = Properties()
+
+        versionProps.load(versionPropsFile.inputStream())
+        val name = versionProps.getProperty("VERSION_NAME")
+        val code = versionProps.getProperty("VERSION_CODE").toInt() + 1
+        val build = versionProps.getProperty("VERSION_BUILD").toInt() + 1
+
+        defaultConfig {
+            applicationId = "cu.uci.porter"
+            minSdkVersion(21)
+            targetSdkVersion(28)
+            versionCode = code
+            versionName = "$name.$build"
+            testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+            multiDexEnabled = true
+            vectorDrawables.useSupportLibrary = true
+        }
+    } else {
+        throw GradleException("Could not read version.properties!")
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("porter-key.jks")
+            storePassword = "porterkey"
+            keyAlias = "porter"
+            keyPassword = "porterkey"
+        }
+    }
+
+    buildTypes {
+
+        getByName("release") {
+            isDebuggable = false
+            isMinifyEnabled = false
+            proguardFile(getDefaultProguardFile("proguard-android.txt"))
+            proguardFile("proguard-rules.pro")
+
+            signingConfig = signingConfigs.getByName("release")
+            resValue("string", "content_provider", "cu.uci.porter.android.provider")
+        }
+    }
+
+    applicationVariants.all {
+        println("applicationVariants: ${this.name}")
+
+        if (buildType.name == "release") {
+            updateVersionCode()
+        }
+
+        outputs.map { it as BaseVariantOutputImpl }.forEach {
+            it.outputFileName = it.outputFileName
+                .replace("app-", "Porter@-")
+                .replace(".apk", "-${versionName}.${versionCode}.apk")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}
+
+kapt {
+    generateStubs = true
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.71")
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.core:core-ktx:1.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.preference:preference:1.1.0")
+    testImplementation("junit:junit:4.13")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+
+    implementation("com.google.android.material:material:1.2.0-alpha05")
+
+    implementation("com.karumi:dexter:6.0.2")
+
+    implementation("androidx.room:room-runtime:2.2.5")
+    kapt("androidx.room:room-compiler:2.2.5")
+    implementation("androidx.room:room-ktx:2.2.5")
+    implementation("androidx.room:room-rxjava2:2.2.5")
+
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
+
+    implementation("com.google.code.gson:gson:2.8.6")
+
+    implementation("com.google.dagger:dagger-android:2.27")
+    implementation("com.google.dagger:dagger-android-support:2.27")
+    kapt("com.google.dagger:dagger-android-processor:2.27")
+    kapt("com.google.dagger:dagger-compiler:2.27")
+
+    implementation("androidx.fragment:fragment-ktx:1.2.4")
+    implementation("androidx.fragment:fragment-ktx:1.2.4")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.3.0-alpha04")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.0-alpha04")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.0-alpha04")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.71")
+
+    implementation("com.jakewharton.timber:timber:4.7.1")
+
+    implementation("org.ocpsoft.prettytime:prettytime:4.0.1.Final")
+
+    implementation("io.reactivex.rxjava3:rxjava:3.0.0")
+
+    /**circular imagenView*/
+    implementation("com.mikhaellopez:circularimageview:3.2.0")
+
+    /**RX*/
+    implementation("io.reactivex.rxjava2:rxandroid:2.1.0")
+    implementation("io.reactivex.rxjava2:rxkotlin:2.3.0")
+
+    /**retrofit*/
+    implementation("com.squareup.okhttp3:okhttp:4.4.0")
+    implementation("com.squareup.retrofit2:retrofit:2.8.1")
+    implementation("com.squareup.retrofit2:converter-gson:2.8.1")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.4.0")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.1.0")
+
+    /**Circular progress bar*/
+    implementation("com.github.castorflex.smoothprogressbar:library-circular:1.3.0")
+
+    /** Logger Timber */
+    implementation("com.jakewharton.timber:timber:4.7.1")
+
+    /** Glide */
+    implementation("com.github.bumptech.glide:glide:4.9.0")
+
+    /** Qr Reader */
+    implementation("me.dm7.barcodescanner:zxing:1.9.8")
+
+    /**circularProgressBar*/
+    implementation("com.github.castorflex.smoothprogressbar:library-circular:1.3.0")
+
+    /**circular imagenView*/
+    implementation("com.mikhaellopez:circularimageview:3.2.0")
+
+    /**Rounded Buttons*/
+    implementation("com.github.medyo:fancybuttons:1.9.1")
+
+    /**PDF*/
+    implementation("com.itextpdf:itextpdf:5.0.6")
+
+    /**App intro*/
+    implementation("com.github.AppIntro:AppIntro:5.1.0")
+
+    /** Fragmentation */
+    implementation("me.yokeyword:fragmentationx:1.0.1")
+    implementation("me.yokeyword:fragmentationx-swipeback:1.0.1")
+}
+
+fun updateVersionCode() {
+
+    val versionPropsFile = file("version.properties")
+
+    if (versionPropsFile.canRead()) {
+        val versionProps = Properties()
+
+        versionProps.load(versionPropsFile.inputStream())
+        val name = versionProps.getProperty("VERSION_NAME")
+        val code = versionProps.getProperty("VERSION_CODE").toInt() + 1
+        val build = versionProps.getProperty("VERSION_BUILD").toInt() + 1
+
+        versionProps["VERSION_NAME"] = name
+        versionProps["VERSION_BUILD"] = build.toString()
+        versionProps["VERSION_CODE"] = code.toString()
+        versionProps.store(versionPropsFile.outputStream(), null)
+    } else {
+        throw GradleException("Could not read version.properties!")
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.50'
+    val kotlinVersion = "1.3.71"
     repositories {
-
         maven { url = uri("https://maven.aliyun.com/repository/central") }
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/jcenter") }
         maven { url = uri("https://jitpack.io") }
 
-//        mavenCentral()
-//        google()
-//        jcenter()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.6.1'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:3.6.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -24,18 +20,14 @@ buildscript {
 
 allprojects {
     repositories {
-
         maven { url = uri("https://maven.aliyun.com/repository/central") }
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/jcenter") }
         maven { url = uri("https://jitpack.io") }
 
-//        mavenCentral()
-//        google()
-//        jcenter()
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
