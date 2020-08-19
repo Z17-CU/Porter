db98b555d41429d2ba0d191ab7e479054a1567c6
Richard <richard.anca.93@gmail.com>
2020-08-19 09:02:21 -0400

    [ADD] data base migration

--- app/src/main/java/cu/control/queue/repository/dataBase/AppDataBase.kt
+++ app/src/main/java/cu/control/queue/repository/dataBase/AppDataBase.kt
@@ -17,7 +17,7 @@ import cu.control.queue.repository.dataBase.entitys.payload.Person
 
 @Database(
     entities = [(Client::class), (Queue::class), (ClientInQueue::class), (Payload::class), (Person::class)],
-    version = 4
+    version = 5
 )
 @TypeConverters(Converters::class)
 abstract class AppDataBase : RoomDatabase() {
@@ -25,33 +25,9 @@ abstract class AppDataBase : RoomDatabase() {
     abstract fun dao(): Dao
 
     companion object : SingletonHolder<AppDataBase, Context>({
+
         Room.databaseBuilder(it, AppDataBase::class.java, "DataBase.db")
-            .addMigrations(object : Migration(1, 2) {
-                override fun migrate(database: SupportSQLiteDatabase) {
-                    database.execSQL(
-                        "ALTER TABLE ${Client.TABLE_NAME} ADD COLUMN selected INTEGER"
-                    )
-                    database.execSQL(
-                        "ALTER TABLE ${Client.TABLE_NAME} ADD COLUMN searched INTEGER"
-                    )
-                    database.execSQL(
-                        "ALTER TABLE ${Queue.TABLE_NAME} ADD COLUMN description TEXT NOT NULL DEFAULT ''"
-                    )
-                }
-            })
-            .addMigrations(object : Migration(2, 3) {
-                override fun migrate(database: SupportSQLiteDatabase) {
-                    database.execSQL(
-                        "ALTER TABLE ${Client.TABLE_NAME} ADD COLUMN onBlackList INTEGER"
-                    )
-                    database.execSQL(
-                        "ALTER TABLE ${Client.TABLE_NAME} ADD COLUMN repeatedClient INTEGER"
-                    )
-                    database.execSQL(
-                        "ALTER TABLE ${ClientInQueue.TABLE_NAME} ADD COLUMN repeatedClient INTEGER"
-                    )
-                }
-            })
+            .fallbackToDestructiveMigration()
             .build()
     })
 }
